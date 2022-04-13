1ddb593777f65ed8aec9c01b24017707bc5666fb
Krishna Pandian <kpandian@ucsc.edu>
2022-04-12 18:23:34 -0700

    Removing token

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import requests
 res = requests.get("https://raw.github.com/kpandian-ucsc/dummy-ci/main/cloud-formation.yaml")
 print(res.text)
 
-# dapi1a2b3c45d67890e1f234567a8bc9012d
+# 
 
 
     # "https://raw.github.com/kpandian-ucsc/dummy-ci/main/cloud-formation.yaml"
